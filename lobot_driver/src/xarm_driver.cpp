a2ca581e5a36ccf7bf2b414a858c24b6a91ca0ec
Jiaxin Shou <shoujiaxin@gmail.com>
2019-03-07 22:48:07 +0800

    Update driver for xArm

--- lobot_driver/src/xarm_driver.cpp
+++ lobot_driver/src/xarm_driver.cpp
@@ -1,78 +1,77 @@
 #include "lobot_driver/xarm_driver.h"
-#include <controller_manager/controller_manager.h>
+
 #include <ros/ros.h>
-#include <iostream>
+#include <sensor_msgs/JointState.h>
 
 XArmDriver::XArmDriver(const unsigned short vendorId,
-                       const unsigned short productId) {
+                       const unsigned short productId, ros::NodeHandle& nh)
+    : as_(nh, "xarm/xarm_joint_controller/follow_joint_trajectory",
+          boost::bind(&XArmDriver::write, this, _1), false),
+      jntStatePub_(
+          nh_.advertise<sensor_msgs::JointState>("xarm/joint_states", 1)),
+      myHid(std::make_shared<MyHid>(vendorId, productId)) {
+  // start follow_joint_trajectory action server
+  as_.start();
+
   // connect and register the joint state interface
-  hardware_interface::JointStateHandle state_handle_1(
-      "/xarm/arm_joint1", &pos[0], &vel[0], &eff[0]);
-  jnt_state_interface.registerHandle(state_handle_1);
-
-  hardware_interface::JointStateHandle state_handle_2(
-      "/xarm/arm_joint2", &pos[1], &vel[1], &eff[1]);
-  jnt_state_interface.registerHandle(state_handle_2);
-
-  hardware_interface::JointStateHandle state_handle_3(
-      "/xarm/arm_joint3", &pos[2], &vel[2], &eff[2]);
-  jnt_state_interface.registerHandle(state_handle_3);
-
-  hardware_interface::JointStateHandle state_handle_4(
-      "/xarm/arm_joint4", &pos[3], &vel[3], &eff[3]);
-  jnt_state_interface.registerHandle(state_handle_4);
-
-  hardware_interface::JointStateHandle state_handle_5(
-      "/xarm/arm_joint5", &pos[4], &vel[4], &eff[4]);
-  jnt_state_interface.registerHandle(state_handle_5);
-
-  registerInterface(&jnt_state_interface);
+  hardware_interface::JointStateHandle stateHandle1("xarm/arm_joint1", &pos[0],
+                                                    &vel[0], &eff[0]);
+  jntStateInterface_.registerHandle(stateHandle1);
+  hardware_interface::JointStateHandle stateHandle2("xarm/arm_joint2", &pos[1],
+                                                    &vel[1], &eff[1]);
+  jntStateInterface_.registerHandle(stateHandle2);
+  hardware_interface::JointStateHandle stateHandle3("xarm/arm_joint3", &pos[2],
+                                                    &vel[2], &eff[2]);
+  jntStateInterface_.registerHandle(stateHandle3);
+  hardware_interface::JointStateHandle stateHandle4("xarm/arm_joint4", &pos[3],
+                                                    &vel[3], &eff[3]);
+  jntStateInterface_.registerHandle(stateHandle4);
+  hardware_interface::JointStateHandle stateHandle5("xarm/arm_joint5", &pos[4],
+                                                    &vel[4], &eff[4]);
+  jntStateInterface_.registerHandle(stateHandle5);
+  registerInterface(&jntStateInterface_);
 
   // connect and register the joint position interface
-  hardware_interface::JointHandle pos_handle_1(
-      jnt_state_interface.getHandle("/xarm/arm_joint1"), &cmd[0]);
-  jnt_pos_interface.registerHandle(pos_handle_1);
+  hardware_interface::JointHandle posHandle1(
+      jntStateInterface_.getHandle("xarm/arm_joint1"), &cmd[0]);
+  jntPosInterface_.registerHandle(posHandle1);
+  hardware_interface::JointHandle posHandle2(
+      jntStateInterface_.getHandle("xarm/arm_joint2"), &cmd[1]);
+  jntPosInterface_.registerHandle(posHandle2);
+  hardware_interface::JointHandle posHandle3(
+      jntStateInterface_.getHandle("xarm/arm_joint3"), &cmd[1]);
+  jntPosInterface_.registerHandle(posHandle3);
+  hardware_interface::JointHandle posHandle4(
+      jntStateInterface_.getHandle("xarm/arm_joint4"), &cmd[1]);
+  jntPosInterface_.registerHandle(posHandle4);
+  hardware_interface::JointHandle posHandle5(
+      jntStateInterface_.getHandle("xarm/arm_joint5"), &cmd[1]);
+  jntPosInterface_.registerHandle(posHandle5);
+  registerInterface(&jntPosInterface_);
 
-  hardware_interface::JointHandle pos_handle_2(
-      jnt_state_interface.getHandle("/xarm/arm_joint2"), &cmd[1]);
-  jnt_pos_interface.registerHandle(pos_handle_2);
-
-  hardware_interface::JointHandle pos_handle_3(
-      jnt_state_interface.getHandle("/xarm/arm_joint3"), &cmd[1]);
-  jnt_pos_interface.registerHandle(pos_handle_3);
-
-  hardware_interface::JointHandle pos_handle_4(
-      jnt_state_interface.getHandle("/xarm/arm_joint4"), &cmd[1]);
-  jnt_pos_interface.registerHandle(pos_handle_4);
-
-  hardware_interface::JointHandle pos_handle_5(
-      jnt_state_interface.getHandle("/xarm/arm_joint5"), &cmd[1]);
-  jnt_pos_interface.registerHandle(pos_handle_5);
-
-  registerInterface(&jnt_pos_interface);
-
-  myHid = std::make_shared<MyHid>(vendorId, productId);
-}
-
-void XArmDriver::init() {
   // connect the control board via USB-HID
   if (myHid->Open() != 0) {
     return;
   }
-  std::clog << "xArm control board connected!" << std::endl;
+  std::clog << "[INFO] [" << ros::Time::now()
+            << "]: xArm control board connected!" << std::endl;
 
   // initiate all joints
+  SpinServos({1, 2, 3, 4, 5, 6}, {500, 500, 500, 500, 500, 500}, 1800);
+  ros::Duration(2).sleep();
+  std::clog << "[INFO] [" << ros::Time::now() << "]: Arm joints initialized!"
+            << std::endl;
 }
 
-void XArmDriver::read(const ros::Time& time, const ros::Duration& period) {}
-
-void XArmDriver::write(const ros::Time& time, const ros::Duration& period) {}
+XArmDriver::~XArmDriver() { myHid->Close(); }
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "xarm_driver");
 
-  XArmDriver xArmDriver(0x0483, 0x5750);
-  controller_manager::ControllerManager cm(&xArmDriver);
+  ros::NodeHandle nh;
+  XArmDriver xArmDriver(0x0483, 0x5750, nh);
 
-  xArmDriver.init();
+  ros::spin();
+
+  return 0;
 }
